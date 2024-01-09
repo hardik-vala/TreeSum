c4904f08dd912af12b8aa9aed346f12f61c02a43
Hardik Vala <hardikvala24@gmail.com>
2024-01-09 16:10:14 -0500

    Fixes #2.

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,21 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { WorkspaceTreeSummariesProvider } from "./tree";
 
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "treesum" is now active!');
 
-	console.log('Congratulations, your extension "treesum" is now active!');
+  vscode.window.createTreeView("workspaceTreeSummaries", {
+    treeDataProvider: new WorkspaceTreeSummariesProvider(),
+  });
 
-	// The command has been defined in the package.json file.
-	// The commandId parameter must match the command field in package.json.
-	let disposable = vscode.commands.registerCommand('treesum.helloWorld', () => {
-		// Display a message box to the user.
-		vscode.window.showInformationMessage('Hello World from TreeSum!');
-	});
+  // The command has been defined in the package.json file.
+  // The commandId parameter must match the command field in package.json.
+  let disposable = vscode.commands.registerCommand("treesum.helloWorld", () => {
+    // Display a message box to the user.
+    vscode.window.showInformationMessage("Hello World from TreeSum!");
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
