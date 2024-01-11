d70387a38433b886164bfcd76991ad2e87c0b009
Hardik Vala <hardikvala24@gmail.com>
2024-01-11 16:23:27 -0500

    Drop sample command.

--- src/extension.ts
+++ src/extension.ts
@@ -11,17 +11,10 @@ export function activate(context: vscode.ExtensionContext) {
   vscode.window.createTreeView("workspaceTreeSummaries", {
     treeDataProvider: workspaceTreeSummariesProvider,
   });
-  vscode.commands.registerCommand("treesum.refresh", () => {
+  let disposable = vscode.commands.registerCommand("treesum.refresh", () => {
     workspaceTreeSummariesProvider.refresh();
 		vscode.window.showInformationMessage("Refreshed file and folder summaries.");
 	});
-
-  // The command has been defined in the package.json file.
-  // The commandId parameter must match the command field in package.json.
-  let disposable = vscode.commands.registerCommand("treesum.helloWorld", () => {
-    // Display a message box to the user.
-    vscode.window.showInformationMessage("Hello World from TreeSum!");
-  });
 
   context.subscriptions.push(disposable);
 }
